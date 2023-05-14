6176232636a8a534f53ca8946cbc645e17385f59
Salatiel Galvão Zambelino <84423199+salgal42@users.noreply.github.com>
2023-05-14 20:35:15 -0300

    Adicionei arquivos via upload

---
+++ MediaAritmetica Github/src/br/com/salgal42/git/MediaAritmeticaGit.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package br.com.salgal42.git;
+
+/**
+ * @author Salatiel Galvão Zambelino
+ * Média Aritmética em os números 10, 7 e 8
+ */
+public class MediaAritmeticaGit {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 7;
+		int n3 = 8;
+		double media;
+				
+		media = (n1+n2+n3)/3.0;
+		System.out.printf("A média é: %.2f%n",media);	
+
+	}
+
+}
